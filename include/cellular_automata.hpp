571c94a054ae2950e2b84c1534b20543c8c5cbfe
r4d2 <patrick.flick@gmail.com>
2015-03-25 00:05:34 -0400

    started with border_matrix (to be continued)

--- include/cellular_automata.hpp
+++ include/cellular_automata.hpp
@@ -14,8 +14,8 @@
 #include <cstdlib>
 #include <assert.h>
 #include <algorithm>
+#include <vector>
 
-// TODO: generalize the border size
 
 // simple matrix type, data saved as row-major
 template <typename T>
@@ -24,14 +24,25 @@ public:
     typedef T value_type;
 
     /// Default constructor
-    matrix() : m(0), n(0), data() {}
+    matrix() : m(0), n(0), m_data() {}
 
     /// Copy constructor
-    matrix(const matrix<T>& o) : m(o.m), n(o.n), data(o.data) {}
+    matrix(const matrix<T>& o) : m(o.m), n(o.n), m_data(o.m_data) {}
 
     /// emtpy initialization
     matrix(std::size_t nrows, std::size_t ncols)
-        : m(nrows), n(ncols), data(n*m) {}
+        : m(nrows), n(ncols), m_data(n*m) {}
+
+    /// initialization with data
+    matrix(std::size_t nrows, std::size_t ncols, const std::vector<T>& data)
+        : m(nrows), n(ncols), m_data(data) {}
+
+    /// initialization with data from iterators
+    template <typename InputIterator>
+    matrix(std::size_t nrows, std::size_t ncols, InputIterator begin, InputIterator end)
+        : m(nrows), n(ncols), m_data(begin, end) {
+        assert(std::distance(begin, end) == n*m);
+    }
 
     /// Destructor
     virtual ~matrix() {}
@@ -40,23 +51,38 @@ public:
     virtual matrix<T>& operator=(const matrix<T>& other) {
         m = other.m;
         n = other.n;
-        data = other.data;
+        m_data = other.m_data;
     }
 
     /// element access operator
-    const T& operator()(std::size_t i, std::size_t j) const
+    inline const T& operator()(std::size_t i, std::size_t j) const
     {
         assert(0 <= i && i < m);
         assert(0 <= j && j < n);
-        return data[i*n + j];
+        return m_data[i*n + j];
     }
 
     /// element access operator
-    T& operator()(std::size_t i, std::size_t j)
+    inline T& operator()(std::size_t i, std::size_t j)
     {
         assert(0 <= i && i < m);
         assert(0 <= j && j < n);
-        return data[i*n + j];
+        return m_data[i*n + j];
+    }
+
+    /// returns a raw pointer to the underlying data
+    inline T* data() {
+        return &m_data[0];
+    }
+
+    /// Returns the number of rows
+    inline std::size_t nrows() const {
+        return m;
+    }
+
+    /// Returns the number of columns
+    inline std::size_t ncols() const {
+        return n;
     }
 
 protected:
@@ -64,11 +90,107 @@ protected:
     const std::size_t m;
     /// number columns
     const std::size_t n;
-    std::vector<T> data;
+    /// data of the matrix
+    std::vector<T> m_data;
 };
 
 // matrix with border (generalized)
-// TODO
+// TODO the floor field model will consist of multiple of these!
+// simple matrix type, data saved as row-major
+template <typename T>
+class border_matrix : protected matrix<T> {
+public:
+    typedef T value_type;
+    typedef matrix<T> matrix_type;
+
+    /// Default constructor
+    border_matrix() : matrix_type() {}
+
+    /// Copy constructor
+    border_matrix(const border_matrix<T>& o)
+        : matrix_type(o),
+          border_size(o.border_size) {}
+
+    /// emtpy initialization
+    border_matrix(std::size_t nrows, std::size_t ncols, std::size_t border_size)
+        : matrix_type(nrows+2*border_size, ncols+2*border_size) {}
+
+    /// TODO!!!
+    /// initialization with data from iterators
+    template <typename InputIterator>
+    border_matrix(std::size_t nrows, std::size_t ncols, InputIterator begin, InputIterator end)
+        : m(nrows), n(ncols), m_data(begin, end) {
+        assert(std::distance(begin, end) == n*m);
+    }
+
+    /// initialization with data
+    border_matrix(std::size_t nrows, std::size_t ncols, std::size_t border_size, const std::vector<T>& data)
+        : matrix_type(nrows+2*border_size, ncols+2*border_size) {
+        // two cases: either the vector gives data for the whole grid,
+        // including the border, or only for the internal part
+        if (data.size() == nrows*ncols)
+        {
+            // only data for internal matrix given -> have to copy row by row
+            for (std::size_t i = 0; i < nrows; ++i)
+            {
+                std::copy(&data[i*ncols], &data[(i+1)*ncols],
+                          &this->m_data[i*(this->n) + border_size]);
+            }
+        }
+        else if (data.size() == this->n * this->m)
+        {
+            // data for the whole matrix, including border is given
+            this->m_data = data;
+        }
+    }
+
+
+    /// Destructor
+    virtual ~border_matrix() {}
+
+    /// Assignment operator
+    virtual border_matrix<T>& operator=(const border_matrix<T>& other) {
+        this->m = other.m;
+        this->n = other.n;
+        this->m_data = other.m_data;
+        border_size = other.border_size;
+    }
+
+    /// element access operator
+    inline const T& operator()(std::size_t i, std::size_t j) const
+    {
+        assert(0 <= i && i < this->m);
+        assert(0 <= j && j < this->n);
+        return m_data[i*n + j];
+    }
+
+    /// element access operator
+    inline T& operator()(std::size_t i, std::size_t j)
+    {
+        assert(0 <= i && i < m);
+        assert(0 <= j && j < n);
+        return m_data[i*n + j];
+    }
+
+    /// returns a raw pointer to the underlying data
+    inline T* data() {
+        return &m_data[0];
+    }
+
+    /// Returns the number of rows
+    inline std::size_t nrows() const {
+        return m;
+    }
+
+    /// Returns the number of columns
+    inline std::size_t ncols() const {
+        return n;
+    }
+
+protected:
+    std::size_t border_size;
+};
+
 
 template <typename T>
 class CellularAutomata {
