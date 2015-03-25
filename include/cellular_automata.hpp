19bf03adab8a2f0cc021db40761880556dfcaf9d
r4d2 <patrick.flick@gmail.com>
2015-03-25 12:50:47 -0400

    border_matrix

--- include/cellular_automata.hpp
+++ include/cellular_automata.hpp
@@ -96,7 +96,6 @@ protected:
 
 // matrix with border (generalized)
 // TODO the floor field model will consist of multiple of these!
-// simple matrix type, data saved as row-major
 template <typename T>
 class border_matrix : protected matrix<T> {
 public:
@@ -104,7 +103,7 @@ public:
     typedef matrix<T> matrix_type;
 
     /// Default constructor
-    border_matrix() : matrix_type() {}
+    border_matrix() : matrix_type(), border_size(0) {}
 
     /// Copy constructor
     border_matrix(const border_matrix<T>& o)
@@ -115,35 +114,36 @@ public:
     border_matrix(std::size_t nrows, std::size_t ncols, std::size_t border_size)
         : matrix_type(nrows+2*border_size, ncols+2*border_size) {}
 
-    /// TODO!!!
     /// initialization with data from iterators
     template <typename InputIterator>
     border_matrix(std::size_t nrows, std::size_t ncols, InputIterator begin, InputIterator end)
-        : m(nrows), n(ncols), m_data(begin, end) {
-        assert(std::distance(begin, end) == n*m);
-    }
-
-    /// initialization with data
-    border_matrix(std::size_t nrows, std::size_t ncols, std::size_t border_size, const std::vector<T>& data)
-        : matrix_type(nrows+2*border_size, ncols+2*border_size) {
-        // two cases: either the vector gives data for the whole grid,
-        // including the border, or only for the internal part
-        if (data.size() == nrows*ncols)
+        : matrix_type(nrows+2*border_size, ncols+2*border_size)
+    {
+        std::size_t size = std::distance(begin, end);
+        if (size == nrows*ncols)
         {
             // only data for internal matrix given -> have to copy row by row
             for (std::size_t i = 0; i < nrows; ++i)
             {
-                std::copy(&data[i*ncols], &data[(i+1)*ncols],
+                std::copy(begin+i*ncols, begin + (i+1)*ncols,
                           &this->m_data[i*(this->n) + border_size]);
             }
         }
-        else if (data.size() == this->n * this->m)
+        else if (size == this->n * this->m)
         {
             // data for the whole matrix, including border is given
-            this->m_data = data;
+            std::copy(begin, end, this->m_data.begin());
+        }
+        else
+        {
+            assert(false);
         }
     }
 
+    /// initialization with data
+    border_matrix(std::size_t nrows, std::size_t ncols, std::size_t border_size, const std::vector<T>& data)
+        : border_matrix(nrows, ncols, border_size, data.begin(), data.end()) {
+    }
 
     /// Destructor
     virtual ~border_matrix() {}
@@ -159,32 +159,58 @@ public:
     /// element access operator
     inline const T& operator()(std::size_t i, std::size_t j) const
     {
-        assert(0 <= i && i < this->m);
-        assert(0 <= j && j < this->n);
-        return m_data[i*n + j];
+        assert(0 <= i && i < this->m-2*border_size);
+        assert(0 <= j && j < this->n-2*border_size);
+        return this->m_data[(i+border_size)*this->n + j + border_size];
     }
 
     /// element access operator
     inline T& operator()(std::size_t i, std::size_t j)
     {
-        assert(0 <= i && i < m);
-        assert(0 <= j && j < n);
-        return m_data[i*n + j];
+        assert(0 <= i && i < this->m-2*border_size);
+        assert(0 <= j && j < this->n-2*border_size);
+        return this->m_data[(i+border_size)*this->n + j + border_size];
+    }
+
+    /// element access operator with `at(i,j)` (otherwise identical)
+    inline const T& at(std::size_t i, std::size_t j) const
+    {
+        assert(0 <= i && i < this->m-2*border_size);
+        assert(0 <= j && j < this->n-2*border_size);
+        return this->m_data[(i+border_size)*this->n + j + border_size];
+    }
+
+    /// element access operator with `at(i,j)` (otherwise identical)
+    inline T& at(std::size_t i, std::size_t j)
+    {
+        assert(0 <= i && i < this->m-2*border_size);
+        assert(0 <= j && j < this->n-2*border_size);
+        return this->m_data[(i+border_size)*this->n + j + border_size];
     }
 
     /// returns a raw pointer to the underlying data
     inline T* data() {
-        return &m_data[0];
+        return &this->m_data[0];
+    }
+
+    /// Returns the matrix including border
+    inline matrix_type& mat() {
+        return *((matrix_type*) this);
     }
 
     /// Returns the number of rows
     inline std::size_t nrows() const {
-        return m;
+        return this->m - 2*border_size;
     }
 
     /// Returns the number of columns
     inline std::size_t ncols() const {
-        return n;
+        return this->n - 2*border_size;
+    }
+
+    /// Returns the size of the border
+    inline std::size_t bordersize() const {
+        return border_size;
     }
 
 protected:
